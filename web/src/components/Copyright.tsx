4ffe35d8d440affb4556bb62f8737a35b4e81e5d
TiagoM13 <thyagomotha2000@gmail.com>
2023-05-23 12:57:35 -0300

    separating application into components

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Copyright = () => {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 no NLW da{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        className="underline hover:text-gray-100"
+        href="https://app.rocketseat.com.br"
+      >
+        Rocketseat
+      </a>
+    </div>
+  )
+}
