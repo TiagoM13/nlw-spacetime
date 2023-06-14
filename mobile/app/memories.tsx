fd5038aaffda802ab4c4174027ea67cd68e68afd
TiagoM13 <thyagomotha2000@gmail.com>
2023-06-13 21:45:36 -0300

    creating memory list screen, logout button

--- mobile/app/memories.tsx
+++ mobile/app/memories.tsx
@@ -1,9 +1,45 @@
-import { View, Text } from 'react-native'
+import React from 'react'
+import { View, TouchableOpacity, ScrollView } from 'react-native'
+import Icon from '@expo/vector-icons/Feather'
+import { Link, useRouter } from 'expo-router'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import * as SecureStore from 'expo-secure-store'
 
-export default function Memories() {
+import NLWlogo from '../src/assets/nlw-spacetime-logo.svg'
+
+export default function NewMemories() {
+  const router = useRouter()
+  const { bottom, top } = useSafeAreaInsets()
+
+  async function signOut() {
+    await SecureStore.deleteItemAsync('token')
+
+    router.push('/')
+  }
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Memories</Text>
-    </View>
+    <ScrollView
+      className="flex-1 px-8"
+      contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
+    >
+      <View className="mt-4 flex-row items-center justify-between">
+        <NLWlogo />
+
+        <View className="flex-row gap-2">
+          <TouchableOpacity
+            onPress={signOut}
+            className="h-10 w-10 items-center justify-center rounded-full bg-red-500"
+          >
+            <Icon name="log-out" size={16} color="#000" />
+          </TouchableOpacity>
+
+          <Link href="/new" asChild>
+            <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-green-500">
+              <Icon name="plus" size={16} color="#FFF" />
+            </TouchableOpacity>
+          </Link>
+        </View>
+      </View>
+    </ScrollView>
   )
 }
