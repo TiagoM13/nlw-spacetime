dec6f76fbb1964a938615f97d9a4918e4f3c5a9d
TiagoM13 <thyagomotha2000@gmail.com>
2023-06-14 21:51:51 -0300

    add scroll right

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
             <Copyright />
           </div>
 
-          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+          <div className="flex max-h-screen flex-col overflow-x-scroll bg-[url(../assets/bg-stars.svg)] bg-cover">
             {children}
           </div>
         </main>
